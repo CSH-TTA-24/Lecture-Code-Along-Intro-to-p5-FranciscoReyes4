132e4f3d9b89e625060e76491e93a4a3089254d3
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 09:56:45 -0400

    Initial Commit

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(500, 500);
+  background(220, 220, 220);
+  point(20,20);
+   fill(255, 0, 0);
+  stroke(255,0,255);
+  strokeWeight(10);
+  line(100,100,400,400);
+  ellipse(180,200,200,100);
+  rect(200,200,120,190)
+  
+}
